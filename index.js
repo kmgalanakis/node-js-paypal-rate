e794cbfa89778eab9aae959a1ad90223afeea1e6
Konstantinos Galanakis <kmgalanakis@gmail.com>
2017-05-15 11:18:14 +0300

    Fix a port related issue that persists.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var paypalModule = require( './modules/paypal.js' );
 var express = require( 'express' );
 var app = express();
 
-app.set( 'port', ( process.env.Port || 30000 ) );
+app.set( 'port', ( process.env.PORT ) );
 app.use( express.static( __dirname + '/public' ) );
 
 app.get( '/googleapitoken', function( request, response ) {
